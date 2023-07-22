c51fdb5622d20c9aa81ed74e605344a525d77f54
Steffen R. Knollmann <srk@k7n.dev>
2023-07-22 07:08:50 +0100

    feat(bash): disable the ** recursion again
    Not quite sure why, must have interfered with something I was doing and
    I haven't actually missed not having it.

--- things/bash/config/shopt.bash
+++ things/bash/config/shopt.bash
@@ -2,4 +2,4 @@ shopt -s histappend  # Append to histfile on exist instead of replace
 shopt -s cmdhist  # Save multi-line commands as single line in history
 shopt -s checkwinsize  # Update window size (col/rows) after commands
 shopt -s no_empty_cmd_completion  # Don't tab-complete empty lines
-shopt -s globstar  # Enable ** recursion
+#shopt -s globstar  # Enable ** recursion
